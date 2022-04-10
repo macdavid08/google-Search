473fd4395358c98f0681c3a410b2c816a526d8a8
macdavid08 <99491939+macdavid08@users.noreply.github.com>
2022-04-11 04:32:19 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let input = document.querySelector('.input')
+let icon = document.querySelector('.search')
+let body = document.querySelector('.body')
+
+input.addEventListener('click', ()=> {
+    icon.style.visibility = 'hidden'
+    
+    
+})
+
+body.addEventListener('click', ()=> {
+    icon.style.visibility = 'visible'
+    icon.style.marginLeft = '420px'
+})
